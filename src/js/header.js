920ce498795a4301cdbcdd5885178a6a246341ef
Liliana Canpor <liliana.canpor@gmail.com>
2017-09-07 19:21:18 -0500

    #4 _ header.js_ fixed convention issue

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,14 @@
-if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "project-more/index.html" || window.location.pathname === "project-more/") {
-  
-  var siteNavbar = document.querySelector('.site-navbar');
+var siteNavbar = document.querySelector('.site-navbar');
 
-  
-
-  window.addEventListener('scroll', changeNavSt);
-
-  function changeNavSt() {
-    if (window.scrollY > 20) {
-      siteNavbar.classList.remove('active');
-    } else {
-      siteNavbar.classList.add('active');
-    }
+function changeNavSt() {
+  if (window.scrollY > 20) {
+    siteNavbar.classList.remove('active');
+  } else {
+    siteNavbar.classList.add('active');
   }
+}
+if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "project-more/index.html" || window.location.pathname === "project-more/") {
+  window.addEventListener('scroll', changeNavSt);
   changeNavSt();
 }
+

9866a40cffc852450e5d59f3ec1f9226848b8082
Liliana Canpor <liliana.canpor@gmail.com>
2017-09-12 18:12:36 -0500

    #16 changed properties to homepage-services

---
+++ src/js/homepage_services.js
@@ -0,0 +1,21 @@
+
+function showTab(service) {
+
+  var i, tabcontent, tablinks;
+  tablinks = document.getElementsByClassName("section_services-tablink");
+  tabcontent = document.getElementsByClassName("section_services-tabcontent-wrapper");
+
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" open-tab", "");
+  }
+
+  document.getElementById(service).style.display = "flex";
+  // e.currentTarget.className += " open-tab";
+}
+
+// // Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultTab").click();

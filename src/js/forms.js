58fd46de6dab0c39edd3c0ed4a6f78cc761ae5be
soycamis <camiilososa@gmail.com>
2017-09-27 16:01:26 -0500

    #52 function applied only to certain sections and forms

---
+++ src/js/forms.js
@@ -0,0 +1,29 @@
+(function () {
+
+  "use strict"
+
+
+  if (window.location.pathname === "/" ||
+      window.location.pathname === "/index.html" ||
+      window.location.pathname === "/project-more/index.html" ||
+      window.location.pathname === "/careers.html" ||
+      window.location.pathname === "/project-more/careers.html" ||
+      window.location.pathname === "/referrals.html" ||
+      window.location.pathname === "/project-more/referrals.html") {
+
+        var inputs = $( "input" ).not( "#form-contact-more_desktop input" );
+        var labelChange;
+        
+        labelChange = function(idElement) {
+          $('#' + idElement).change(function () {
+            $('label[for="' + idElement + '"]').text($('#' + idElement).val());
+            $('#' + idElement).css('visibility', 'hidden');
+          });
+        }
+
+        inputs.map(function(index, elem) {
+          console.log(elem.id);
+          labelChange(elem.id);
+        });
+      }
+})();

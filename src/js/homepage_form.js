e0d60584b53b0fc1c0363df0f6035fdd3505316f
Liliana Canpor <liliana.canpor@gmail.com>
2017-10-17 18:19:31 -0500

    #90 homepage-form form validation

--- src/js/homepage_form.js
+++ src/js/homepage_form.js
@@ -19,34 +19,18 @@ $(function () {
 });
 
 $('.section_form-carousel-next').eq(0).on("click", function() {
-  
-    var inputName = $('#inputName').val();
-  
-    $('#nameText').text(inputName);
-  });
-  
-  $('.section_form-carousel-next').eq(1).on("click", function() {
-    var inputEmail = $('#inputEmail').val();
-    $('#emailText').text(inputEmail);
-  });
+  var inputName = $('#inputName').val();
+  $('#nameText').text(inputName);
+});
+
+$('.section_form-carousel-next').eq(1).on("click", function() {
+  var inputEmail = $('#inputEmail').val();
+  $('#emailText').text(inputEmail);
+});
 
 // Validation form contact in home_desktop
 
 $('form[name="form-contact-more_desktop"]').validate({
-  // required fields
-  name: 'required',
-  message: 'required',
-  email: {
-    required: true,
-    // Specify that email should be validated
-    // by the built-in "email" rule
-    email: true
-  },
-  messages: {
-    name: "Por favor introduce tu nombre.",
-    message: "Por favor introduce tu mensaje.",
-    email: "Por favor introduce un correo válido."
-  },
   submitHandler: function(form) {
     var data = $('form[name="form-contact-more_desktop"]').serialize();
     $.ajax({
@@ -65,26 +49,55 @@ $('form[name="form-contact-more_desktop"]').validate({
     var errors = validator.numberOfInvalids();
     alertify.logPosition("bottom right");
     alertify.error("Verifica tu información, tienes " + errors + " errores.");
-  }
+  },
+  rules:('add', {
+    name: {
+      required: true,
+      regex: /^([a-zA-Z]+[,.]?[ ]?|[a-zA-Z]+['-]?)+$/
+    },
+    message: {
+      required: true
+    },
+    email: {
+      required: true,
+      // Specify that email should be validated
+      // by the built-in "email" rule
+      email: true
+    },
+    messages: {
+      name: {
+        required: "Por favor introduce tu nombre ",
+        regex: "Por favor introduce un nombre válido"
+      },
+      message: "Por favor introduce tu mensaje.",
+      email: "Por favor introduce un correo valido."
+    }
+  })
+  
 });
+
+//------------------ getmore.mx - @soycamis
+// added function to capture TAB and ENTER to make the field change
+// in the form of the homepage as it, is a slider.
+// -----------------------------------------------------------------
+
+var captureTabandEnter = function(event){
+  if ( event.which === 13 || event.which === 9) {
+   var form = $('form[name="form-contact-more_desktop"]').validate();
+    event.preventDefault();
+    if ( form.element(this) ) { $('.section_form-carousel').slick('slickNext'); }
+    if (this.id === 'inputName'){ $('#nameText').text($(this).val()); }
+    if (this.id === 'inputEmail'){$('#emailText').text($(this).val()); }
+    if (this.id === 'inputMessage') { $('form[name="form-contact-more_desktop"]').submit(); }
+  }
+ }
+ 
+ var elementsInput = $('form[name="form-contact-more_desktop"] input');
+ elementsInput.on('keydown', captureTabandEnter);
 
 // Validation form contact in home_mobil
 
 $('form[name="form-contact-more_mobile"]').validate({
-  // required fields
-  name: 'required',
-  message: 'required',
-  email: {
-    required: true,
-    // Specify that email should be validated
-    // by the built-in "email" rule
-    email: true
-  },
-  messages: {
-    name: "Por favor introduce tu nombre.",
-    message: "Por favor introduce tu mensaje.",
-    email: "Por favor introduce un correo valido."
-  },
   submitHandler: function(form) {
     var data = $('form[name="form-contact-more_mobile"]').serialize();
     $.ajax({
@@ -104,26 +117,37 @@ $('form[name="form-contact-more_mobile"]').validate({
     var errors = validator.numberOfInvalids();
     alertify.logPosition("bottom right");
     alertify.error("Verifica tu información tienes " + errors + " errores.");
-  }
+  },
+  // required fields
+  rules:('add', {
+    name: {
+      required: true,
+      regex: /^([a-zA-Z]+[,.]?[ ]?|[a-zA-Z]+['-]?)+$/
+    },
+    message: {
+      required: true
+    },
+    email: {
+      required: true,
+      // Specify that email should be validated
+      // by the built-in "email" rule
+      email: true
+    },
+    messages: {
+      name: {
+        required: "Por favor introduce tu nombre ",
+        regex: "Por favor introduce un nombre válido"
+      },
+      message: "Por favor introduce tu mensaje.",
+      email: "Por favor introduce un correo valido."
+    }
+  })
 });
 
 // Validate form contact in section contact
 
 $('form[name="form-contact-more_contact"]').validate({
-  // required fields
-  name: 'required',
-  message: 'required',
-  email: {
-    required: true,
-    // Specify that email should be validated
-    // by the built-in "email" rule
-    email: true
-  },
-  messages: {
-    name: "Por favor introduce tu nombre.",
-    message: "Por favor introduce tu mensaje.",
-    email: "Por favor introduce un correo valido."
-  },
+  
   submitHandler: function(form) {
     var data = $('form[name="form-contact-more_contact"]').serialize();
     $.ajax({
@@ -143,5 +167,28 @@ $('form[name="form-contact-more_contact"]').validate({
     var errors = validator.numberOfInvalids();
     alertify.logPosition("bottom right");
     alertify.error("Verifica tu información tienes " + errors + " errores.");
-  }
+  },
+  rules:('add', {
+    name: {
+      required: true,
+      regex: /^([a-zA-Z]+[,.]?[ ]?|[a-zA-Z]+['-]?)+$/
+    },
+    message: {
+      required: true
+    },
+    email: {
+      required: true,
+      // Specify that email should be validated
+      // by the built-in "email" rule
+      email: true
+    },
+    messages: {
+      name: {
+        required: "Por favor introduce tu nombre ",
+        regex: "Por favor introduce un nombre válido"
+      },
+      message: "Por favor introduce tu mensaje.",
+      email: "Por favor introduce un correo valido."
+    }
+  })
 });

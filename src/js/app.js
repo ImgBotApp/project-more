64dda1ac7319b1bbec2d937a56d62181ca9e8272
Liliana Canpor <liliana.canpor@gmail.com>
2017-10-17 18:18:22 -0500

    #90 app.js form validation

--- src/js/app.js
+++ src/js/app.js
@@ -43,4 +43,16 @@
       });
     }
   });
+  $.validator.addMethod(
+    "regex",
+    function(value, element, regexp) 
+    {
+      if (regexp.constructor != RegExp)
+          regexp = new RegExp(regexp);
+      else if (regexp.global)
+          regexp.lastIndex = 0;
+      return this.optional(element) || regexp.test(value);
+    },
+    "Por favor escribe un campo válido"
+  );
 })();

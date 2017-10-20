220a0a3a6891369c9a0c2a450e93b7110e6ea992
Liliana Canpor <liliana.canpor@gmail.com>
2017-10-20 17:48:49 -0500

    #96 projects-home-carousel changes

--- src/js/homepage_projects_carousel.js
+++ src/js/homepage_projects_carousel.js
@@ -4,27 +4,29 @@
   $('.section_projects-slick-firstrow').slick({
     dots: false,
     infinite: true,
-    cssEase: 'linear',
-    autoplaySpeed: 0,
-    speed: 5000,
-    slidesToShow: 2,
-    centerMode: false,
+    // cssEase: 'linear',
+    // autoplaySpeed: 0,
+    // speed: 5000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
     variableWidth: true,
-    adaptiveHeight: true,
+    // adaptiveHeight: true,
     arrows: false,
-    autoplay: true
+    autoplay: false,
   });
   $('.section_projects-slick-secondrow').slick({
     dots: false,
     infinite: true,
-    cssEase: 'linear',
-    autoplaySpeed: 0,
-    speed: 6000,
-    slidesToShow: 2,
-    centerMode: false,
+    // cssEase: 'linear',
+    // autoplaySpeed: 0,
+    // speed: 6000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
     variableWidth: true,
-    adaptiveHeight: true,
+    // adaptiveHeight: true,
     arrows: false,
-    autoplay: true
+    autoplay: false,
   });
 })();

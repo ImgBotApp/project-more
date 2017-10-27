7590d1d0762f42a86d4c154a35e9229245c72b38
Liliana Canpor <liliana.canpor@gmail.com>
2017-10-27 14:22:41 -0500

    #98 include index-es for dynamic menu

--- src/js/header.js
+++ src/js/header.js
@@ -7,7 +7,7 @@ function changeNavSt() {
     siteNavbar.classList.add('active');
   }
 }
-if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "/project-more/index.html" || window.location.pathname === "/project-more/") {
+if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "/project-more/index.html" || window.location.pathname === "/project-more/" || window.location.pathname === "/index-es.html" || window.location.pathname === "/project-more/index-es.html") {
   window.addEventListener('scroll', changeNavSt);
   changeNavSt();
 }

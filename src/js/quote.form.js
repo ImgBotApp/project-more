bea55f999583c1ccd67cd5aa4d52f53a13e331d9
Liliana Canpor <liliana.canpor@gmail.com>
2017-10-27 14:23:04 -0500

    #98 spanish validations succeded

--- src/js/quote.form.js
+++ src/js/quote.form.js
@@ -1,3 +1,4 @@
+var currentFile = window.location.pathname;
 $('form[name="form-quote"]').validate({
   budgetQuote:'required',
   submitHandler: function(form) {
@@ -9,7 +10,11 @@ $('form[name="form-quote"]').validate({
     }).done(function(data) {
       if (parseInt(data) === 1) {
         alertify.logPosition("bottom right");
-        alertify.success("Thanks for contacting us!");
+        if (currentFile === '/' || currentFile === '/portfolio.html' || currentFile === '/services.html' || currentFile === '/careers.html' || currentFile === '/team.html' || currentFile === '/referrals.html' || currentFile === '/quote.html' || currentFile === '/contact.html'){
+          alertify.success("Thanks for contacting us!");
+        }else if (currentFile === '/index-es.html' || currentFile === '/portfolio-es.html' || currentFile === '/services-es.html' || currentFile === '/careers-es.html' || currentFile === '/team-es.html' || currentFile === '/referrals-es.html' || currentFile === '/quote-es.html' || currentFile === '/contact-es.html'){
+          alertify.success("¡Gracias por contactarnos!");
+        }
         $('form[name="form-quote"]')[0].reset();
       }
     });
@@ -17,7 +22,11 @@ $('form[name="form-quote"]').validate({
   invalidHandler: function(event, validator) {
     var errors = validator.numberOfInvalids();
     alertify.logPosition("bottom right");
-    alertify.error("Verify your information, you have " + errors + " errors.");
+    if (currentFile === '/' || currentFile === '/portfolio.html' || currentFile === '/services.html' || currentFile === '/careers.html' || currentFile === '/team.html' || currentFile === '/referrals.html' || currentFile === '/quote.html' || currentFile === '/contact.html'){
+      alertify.error("Verify your information, you have " + errors + " errors.");
+    }else if (currentFile === '/index-es.html' || currentFile === '/portfolio-es.html' || currentFile === '/services-es.html' || currentFile === '/careers-es.html' || currentFile === '/team-es.html' || currentFile === '/referrals-es.html' || currentFile === '/quote-es.html' || currentFile === '/contact-es.html'){
+      alertify.error("Verifica tu infomación, tienes " + errors + " errores.");
+    }
   },
 
   rules:('add', {

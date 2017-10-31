d27ad67e6436334780dce396ef7f0251279f39ec
Liliana Canpor <liliana.canpor@gmail.com>
2017-10-31 12:29:08 -0600

    #110 add cover class to lang menu

--- src/js/lang_menu.js
+++ src/js/lang_menu.js
@@ -1,5 +1,11 @@
 var langMenu = document.querySelector('.lang_menu_desktop');
 var langMenuMobile = document.querySelector('.lang_mobile');
 
-langMenu.addEventListener('click', function () { langMenu.classList.toggle('active')});;
-langMenuMobile.addEventListener('click', function () { langMenuMobile.classList.toggle('active')});
+langMenu.addEventListener('click', function () { 
+  langMenu.classList.toggle('active');
+  document.querySelector('.cover-lang_menu-d').classList.toggle('coverOn');
+});
+langMenuMobile.addEventListener('click', function () { 
+  langMenuMobile.classList.toggle('active');
+  document.querySelector('.cover-lang_menu-m').classList.toggle('coverOn');
+});

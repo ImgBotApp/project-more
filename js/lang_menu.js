4e1d29ec6ed1481739dfe8e224a176d8aefb4f46
Juan Resendiz <jresendiz27@gmail.com>
2017-10-31 17:03:48 -0600

    Update 2017-10-31T23:03:45.652Z

--- js/lang_menu.js
+++
@@ -1,5 +0,0 @@
-var langMenu = document.querySelector('.lang_menu_desktop');
-var langMenuMobile = document.querySelector('.lang_mobile');
-
-langMenu.addEventListener('click', function () { langMenu.classList.toggle('active')});;
-langMenuMobile.addEventListener('click', function () { langMenuMobile.classList.toggle('active')});
--- js/view_services.js
+++
@@ -1,6 +0,0 @@
-$("a.services_view-option-link").click(function() {
-  var targetDiv = $(this).attr('href');
-  $('html, body').animate({
-      scrollTop: $(targetDiv).offset().top - 95
-  }, 800);
-});

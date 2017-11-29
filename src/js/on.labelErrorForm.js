cd0137118eddd860f049c4b297014a2c29af6cc3
Liliana Canpor <liliana.canpor@gmail.com>
2017-11-29 12:50:12 -0600

    #134 rename file

--- src/js/on.labelErrorForm.js
+++
@@ -1,8 +0,0 @@
-
-
-// function to check if a field is invalid and can be modified again.
-
-$(document).on('touchstart click mouseover', 'label.error', function(e) {
-  $('#' + e.target.htmlFor).removeClass('turn-off');
-  $('#' + e.target.htmlFor).addClass('turn-on');
-});
